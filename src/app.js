ad776cd8c923708307a0030599ab6383ff1d55cf
Juan Perez Rodriguez <juaanperez17@gmail.com>
2025-05-26 19:37:04 +0000

    First Commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,19 @@
-import "bootstrap";
-import "./style.css";
+function generateCard() {
+  const suits = ['♦', '♥', '♠', '♣'];
+  const values = ['A','2','3','4','5','6','7','8','9','10','J','Q','K'];
 
+  const suit = suits[Math.floor(Math.random() * suits.length)];
+  const value = values[Math.floor(Math.random() * values.length)];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+  const color = (suit === '♥' || suit === '♦') ? 'red' : 'black';
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+  document.getElementById('top-suit').textContent = suit;
+  document.getElementById('bottom-suit').textContent = suit;
+  document.getElementById('card-value').textContent = value;
+
+  document.getElementById('top-suit').style.color = color;
+  document.getElementById('bottom-suit').style.color = color;
+  document.getElementById('card-value').style.color = color;
+}
+
+window.onload = generateCard;
